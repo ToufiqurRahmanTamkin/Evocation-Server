061f634fdcbc30b590609b0c12d439f0611a54ba
Toufiqru Rahman Tamkin <tamkin22rahman.com>
2021-05-29 17:07:20 +0600

    update features

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import PostMessage from '../models/postMessage.js';
+
 const router = express.Router();
 
 export const getPosts = async (req, res) => { 
